fc451b83a52f7c5b90bab1cbf1ecefff4e165be9
Kasiian Mykhailo <mikhailokasiian8@gmail.com>
2026-03-11 18:26:06 +0200

    Initial commit

---
+++ src/main/java/ua/org/kpik/simple_timetable_generator/SimpleTimetableGeneratorApplication.java
@@ -0,0 +1,13 @@
+package ua.org.kpik.simple_timetable_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTimetableGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTimetableGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/org/kpik/simple_timetable_generator/SimpleTimetableGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.org.kpik.simple_timetable_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTimetableGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
