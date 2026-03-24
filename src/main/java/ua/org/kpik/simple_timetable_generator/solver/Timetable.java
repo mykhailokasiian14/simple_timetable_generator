e2c9c961a7a0f47dfc74ea57dab6eec0de000be7
Kasiian Mykhailo <mikhailokasiian8@gmail.com>
2026-03-24 20:20:03 +0200

    changed class annotation to right and initialized an empty arrayLists for correctly timefold work

--- src/main/java/ua/org/kpik/simple_timetable_generator/solver/Timetable.java
+++ src/main/java/ua/org/kpik/simple_timetable_generator/solver/Timetable.java
@@ -3,6 +3,7 @@ package ua.org.kpik.simple_timetable_generator.solver;
 import ai.timefold.solver.core.api.domain.entity.PlanningEntity;
 import ai.timefold.solver.core.api.domain.solution.PlanningEntityCollectionProperty;
 import ai.timefold.solver.core.api.domain.solution.PlanningScore;
+import ai.timefold.solver.core.api.domain.solution.PlanningSolution;
 import ai.timefold.solver.core.api.domain.solution.ProblemFactCollectionProperty;
 import ai.timefold.solver.core.api.domain.valuerange.ValueRangeProvider;
 import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore;
@@ -12,9 +13,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ua.org.kpik.simple_timetable_generator.entity.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@PlanningEntity
+@PlanningSolution
 @Getter
 @Setter
 @NoArgsConstructor
@@ -33,16 +35,16 @@ public class Timetable {
     private List<Integer> lessonNumbers;
 
     @ProblemFactCollectionProperty
-    private List<Group> groups;
+    private List<Group> groups= new ArrayList<>();
 
     @ProblemFactCollectionProperty
-    private List<Teacher> teachers;
+    private List<Teacher> teachers= new ArrayList<>();
 
     @ProblemFactCollectionProperty
-    private List<Subject> subjects;
+    private List<Subject> subjects= new ArrayList<>();
 
     @ProblemFactCollectionProperty
-    private List<TeachingLoad> teachingLoads;
+    private List<TeachingLoad> teachingLoads = new ArrayList<>();
 
     @PlanningEntityCollectionProperty
     private List<Lesson> lessons;
