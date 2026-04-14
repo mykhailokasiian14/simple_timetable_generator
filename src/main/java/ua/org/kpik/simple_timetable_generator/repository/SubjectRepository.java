ddaccb3f388bebfcf38a6cc8d6a9583e13b2d4df
Kasiian Mykhailo <mikhailokasiian8@gmail.com>
2026-04-14 13:04:10 +0300

    added optional methods for search by names

--- src/main/java/ua/org/kpik/simple_timetable_generator/repository/SubjectRepository.java
+++ src/main/java/ua/org/kpik/simple_timetable_generator/repository/SubjectRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.org.kpik.simple_timetable_generator.entity.Subject;
 
+import java.util.Optional;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findBySubjectName(String subjectName);
 }
--- src/main/java/ua/org/kpik/simple_timetable_generator/repository/TeacherRepository.java
+++ src/main/java/ua/org/kpik/simple_timetable_generator/repository/TeacherRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.org.kpik.simple_timetable_generator.entity.Teacher;
 
+import java.util.Optional;
+
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findByLastName(String lastName);
 }
